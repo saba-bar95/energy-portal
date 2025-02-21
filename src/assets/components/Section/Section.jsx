1defcf0c0306006cf7d912c03544266081e2ee5e
saba-bar95 <s.barbakadze95@gmail.com>
2025-02-21 10:39:45 +0400

    a bit update in section.jsx

--- src/assets/components/Section/Section.jsx
+++ src/assets/components/Section/Section.jsx
@@ -2,17 +2,18 @@
 import sections from "../../../../sections";
 import { useContext, useEffect } from "react";
 import { SectionsContext } from "../../../App";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Section = () => {
   const { sectionID } = useContext(SectionsContext);
   const navigate = useNavigate();
+  const { language } = useParams();
 
   useEffect(() => {
     if (sectionID !== null) {
       const section = sections.find((section) => section.id === sectionID);
       if (section) {
-        navigate(`/ka/${section.href}`);
+        navigate(`/${language}/${section.href}`);
       }
     }
   }, [sectionID, navigate]);
