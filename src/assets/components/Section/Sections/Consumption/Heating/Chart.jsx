d5d7ecf0f180ed54f6c6214f38f3d7bff1e0f164
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-05 15:37:25 +0400

    add heating chart with its styles

---
+++ src/assets/components/Section/Sections/Consumption/Heating/Chart.jsx
@@ -0,0 +1,115 @@
+/* eslint-disable react/prop-types */
+import "./Chart.scss";
+import {
+  BarChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  LabelList,
+} from "recharts";
+import { useParams } from "react-router-dom";
+
+const Chart = ({ data }) => {
+  const { language } = useParams();
+  const sortedData = data.data.sort((a, b) => b.total - a.total);
+
+  const CustomLegend = () => {
+    return (
+      <div className="legend-container">
+        <p>
+          <span style={{ color: data.color[0] }}>■</span>
+          სულ
+        </p>
+        <p>
+          <span style={{ color: data.color[1] }}>■</span>
+          ქალაქად
+        </p>
+        <p>
+          <span style={{ color: data.color[2] }}>■</span>
+          სოფლად
+        </p>
+      </div>
+    );
+  };
+
+  const CustomLabel = (props) => {
+    const { x, y, value } = props;
+    return (
+      <text x={x + 5} y={y - 10} className="text">
+        {value}
+      </text>
+    );
+  };
+
+  const getGeorgianName = (entry) => {
+    const nameMap = {
+      total: "სულ",
+      village: "სოფლად",
+      city: "ქალაქად",
+    };
+    return nameMap[entry.toLowerCase()] || entry;
+  };
+
+  const CustomTooltip = ({ active, payload }) => {
+    if (!active || !payload || !payload.length) return null;
+    return (
+      <div className="custom-tooltip">
+        <div className="tooltip-container">
+          {payload.map(({ name, value, color }, index) => (
+            <p key={`item-${index}`} className="text">
+              <span style={{ color }} className="before-span">
+                ■
+              </span>
+              {language === "en" ? name : getGeorgianName(name)} :
+              <span style={{ fontWeight: 900, marginLeft: "5px" }}>
+                {value.toFixed(1)}
+              </span>
+            </p>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div style={{ width: "100%", height: "100%" }} className="main-chart">
+      <h2>
+        <img src={data.icon} alt="" />
+        {data[`title_${language}`]}
+      </h2>
+      <h3>%</h3>
+      <ResponsiveContainer height={600}>
+        <BarChart data={sortedData} layout="vertical" height={500} barGap={0}>
+          <XAxis
+            type="number"
+            ticks={[0, 20, 40, 60, 80, 100]}
+            tickLine={false}
+          />
+          <YAxis
+            dataKey="name_ge"
+            type="category"
+            tick={false}
+            padding={{ top: 30 }}
+          />
+          <Tooltip content={CustomTooltip} />
+          <Legend content={CustomLegend} />
+          <Bar dataKey="total" fill={data.color[0]} name="Total" barSize={24}>
+            <LabelList dataKey="name_ge" content={CustomLabel} />
+          </Bar>
+          <Bar dataKey="city" fill={data.color[1]} name="City" barSize={24} />
+          <Bar
+            dataKey="village"
+            fill={data.color[2]}
+            name="Village"
+            barSize={24}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default Chart;
