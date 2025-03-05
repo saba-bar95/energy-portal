7f154cf4574d3d95a205eee85877d5ba10cb0411
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-05 15:34:40 +0400

    add heating page

--- src/assets/components/Section/Sections/Consumption/Heating/Heating.jsx
+++ src/assets/components/Section/Sections/Consumption/Heating/Heating.jsx
@@ -1,8 +1,57 @@
+import { useEffect, useState } from "react";
+import fetchLegendCodes from "../../../../../../../fetchData";
+import Chart from "./Chart";
+import firstChartIcon from "/src/assets/images/sections/consumption/1-chart-icon.svg";
+import secondChartIcon from "/src/assets/images/sections/consumption/2-chart-icon.svg";
+
 const Heating = () => {
+  const [chartData, setChartData] = useState([null, null]);
+
+  const chartsConfig = [
+    {
+      householdID: 99,
+      title_ka:
+        "შინამეურნეობების საცხოვრისის განაწილება გათბობის ტიპების მიხედვით",
+      title_en: "Distribution of household dwellings by heating type",
+      icon: firstChartIcon,
+      color: ["#3498DB", "#6CD68C", "#ED4C5C"],
+    },
+    {
+      householdID: 100,
+      title_ka:
+        "შინამეურნეობების წილი გათბობის ინდივიდუალური საშუალებებში გამოყენებული ენერგორესურსების მიხედვით",
+      title_en:
+        "Share of households by energy resources used in individual heating systems",
+      icon: secondChartIcon,
+      color: ["#556EB0", "#6FAEA9", "#FF9F0A"],
+    },
+  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const dataPromises = chartsConfig.map(async (chart) => {
+        const respData = await fetchLegendCodes(chart.householdID);
+        return respData.slice(0, -1);
+      });
+      const results = await Promise.all(dataPromises);
+      setChartData(results);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <h1>This is Heating</h1>
-    </>
+    <div className="heating-container">
+      {chartData.map(
+        (data, index) =>
+          data && (
+            <Chart
+              key={chartsConfig[index].householdID}
+              data={{ ...chartsConfig[index], data }}
+            />
+          )
+      )}
+    </div>
   );
 };
 
