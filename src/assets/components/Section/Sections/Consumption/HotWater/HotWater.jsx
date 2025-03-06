dbb4fd50e1dc7699d0cb49f2bb1298045984fbef
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-06 16:34:01 +0400

    remove unnecessary state

--- src/assets/components/Section/Sections/Consumption/HotWater/HotWater.jsx
+++ src/assets/components/Section/Sections/Consumption/HotWater/HotWater.jsx
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react";
-
 const HotWater = () => {
-  const [year, setYear] = useState(2025);
-
-  useEffect(() => {}, []);
-
   return (
     <>
       <h1>This is Water</h1>
