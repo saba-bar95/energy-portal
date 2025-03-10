f1340e245f7a26316d9b7b975fa21335eb32239b
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-10 17:00:49 +0400

    add download dropdown with its styles and jpg & png downloads

---
+++ src/assets/components/Download/downloadExcel.js
@@ -0,0 +1,26 @@
+// import * as XLSX from "xlsx";
+
+// const downloadExcel = (data, fileName) => {
+//   // Create a new workbook
+//   const workbook = XLSX.utils.book_new();
+
+//   // Convert your data to a worksheet
+//   const worksheet = XLSX.utils.json_to_sheet(data);
+
+//   // Append the worksheet to the workbook
+//   XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+//   // Generate a binary string representation of the workbook
+//   const excelBuffer = XLSX.write(workbook, { bookType: "xlsx", type: "array" });
+
+//   // Create a Blob from the binary string
+//   const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+
+//   // Create a link element to trigger the download
+//   const link = document.createElement("a");
+//   link.href = URL.createObjectURL(blob);
+//   link.download = `${fileName}.xlsx`; // Set the file name
+//   link.click(); // Trigger the download
+// };
+
+// export default downloadExcel;
