fa9fb21ebd79e2b3d6081d6fcf7e4f6e510cf475
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-10 17:15:16 +0400

    add usememo for chartsConfig

--- src/assets/components/Section/Sections/Consumption/Heating/Heating.jsx
+++ src/assets/components/Section/Sections/Consumption/Heating/Heating.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import fetchLegendCodes from "../../../../../../../fetchData";
+import { useEffect, useState, useMemo } from "react";
+import fetchData from "../../../../../../../fetchData";
 import Chart from "./Chart";
 import firstChartIcon from "/src/assets/images/sections/consumption/1-chart-icon.svg";
 import secondChartIcon from "/src/assets/images/sections/consumption/2-chart-icon.svg";
@@ -7,38 +7,41 @@ import secondChartIcon from "/src/assets/images/sections/consumption/2-chart-ico
 const Heating = () => {
   const [chartData, setChartData] = useState([null, null]);
 
-  const chartsConfig = [
-    {
-      householdID: 99,
-      title_ka:
-        "შინამეურნეობების საცხოვრისის განაწილება გათბობის ტიპების მიხედვით",
-      title_en: "Distribution of household dwellings by heating type",
-      icon: firstChartIcon,
-      color: ["#3498DB", "#6CD68C", "#ED4C5C"],
-    },
-    {
-      householdID: 100,
-      title_ka:
-        "შინამეურნეობების წილი გათბობის ინდივიდუალური საშუალებებში გამოყენებული ენერგორესურსების მიხედვით",
-      title_en:
-        "Share of households by energy resources used in individual heating systems",
-      icon: secondChartIcon,
-      color: ["#556EB0", "#6FAEA9", "#FF9F0A"],
-    },
-  ];
+  const chartsConfig = useMemo(
+    () => [
+      {
+        householdID: 99,
+        title_ka:
+          "შინამეურნეობების საცხოვრისის განაწილება გათბობის ტიპების მიხედვით",
+        title_en: "Distribution of household dwellings by heating type",
+        icon: firstChartIcon,
+        color: ["#3498DB", "#6CD68C", "#ED4C5C"],
+      },
+      {
+        householdID: 100,
+        title_ka:
+          "შინამეურნეობების წილი გათბობის ინდივიდუალური საშუალებებში გამოყენებული ენერგორესურსების მიხედვით",
+        title_en:
+          "Share of households by energy resources used in individual heating systems",
+        icon: secondChartIcon,
+        color: ["#556EB0", "#6FAEA9", "#FF9F0A"],
+      },
+    ],
+    []
+  ); // Empty dependency array means it will only be created once
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getData = async () => {
       const dataPromises = chartsConfig.map(async (chart) => {
-        const respData = await fetchLegendCodes(chart.householdID);
+        const respData = await fetchData(chart.householdID);
         return respData.slice(0, -1);
       });
       const results = await Promise.all(dataPromises);
       setChartData(results);
     };
 
-    fetchData();
-  }, []);
+    getData();
+  }, [chartsConfig]);
 
   return (
     <div className="heating-container">
