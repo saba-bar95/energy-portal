14ad206f5cda2188759a0cfce824c425cbc91308
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-12 17:11:09 +0400

    add chart 5

--- src/assets/components/Section/Sections/Visualization/Pages/Resources/Pages/ElectricityAndThermalEnergy/ElectricityAndThermalEnergy.jsx
+++ src/assets/components/Section/Sections/Visualization/Pages/Resources/Pages/ElectricityAndThermalEnergy/ElectricityAndThermalEnergy.jsx
@@ -3,6 +3,7 @@ import Chart_1 from "./Chart_1";
 import Chart_2 from "./Chart_2";
 import Chart_3 from "./Chart_3";
 import Chart_4 from "./Chart_4";
+import Chart_5 from "./Chart_5";
 
 const ElectricityAndThermalEnergy = () => {
   return (
@@ -12,6 +13,7 @@ const ElectricityAndThermalEnergy = () => {
         <Chart_2 />
         <Chart_3 />
         <Chart_4 />
+        <Chart_5 />
       </div>
     </>
   );
