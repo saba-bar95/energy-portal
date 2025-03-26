949aeb7e8f2b27425234527e4ec02a8a0ccee3d0
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-26 10:28:56 +0400

    fix some text errors in footer

--- src/assets/components/Footer/text.js
+++ src/assets/components/Footer/text.js
@@ -4,12 +4,12 @@ const text = {
     header2: "მენიუ",
     header3: "მონაცემთა გამოყენების პირობები",
     header4: "ყველა უფლება დაცულია © საქსტატი 2024",
-    para11: "საქართველოს სტატისტიკის სამსახური",
+    para11: "საქართველოს სტატისტიკის ეროვნული სამსახური",
     para12: "+995 32 2 36 72 10  |  +995 32 2 60 11 60",
     para13: "info@geostat.ge",
     para14: "ცოტნე დადიანის ქ. 30, თბილისი, 0180",
     para15: "სოციალური ქსელები",
-    para2: "ყველა უფლება დაცულია © საქსტატი 2024",
+    para2: "ყველა უფლება დაცულია © საქსტატი 2025",
   },
 
   en: {
@@ -20,9 +20,9 @@ const text = {
     para11: "National Statistics Office of Georgia",
     para12: "+995 32 2 36 72 10  |  +995 32 2 60 11 60",
     para13: "info@geostat.ge",
-    para14: "Tsotne Dadiani Str., 0180, Tbilisi, Georgia",
+    para14: "Tsotne Dadiani Str. 30, 0180, Tbilisi, Georgia",
     para15: "Social networks",
-    para2: "All rights reserved © Geostat 2024",
+    para2: "All rights reserved © Geostat 2025",
   },
 };
 
