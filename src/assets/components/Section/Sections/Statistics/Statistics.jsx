b04927404971c785360a42af6f9a3366238cf633
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-26 14:42:21 +0400

    add statistics page

--- src/assets/components/Section/Sections/Statistics/Statistics.jsx
+++ src/assets/components/Section/Sections/Statistics/Statistics.jsx
@@ -1,7 +1,42 @@
+import { Link, Outlet, useParams } from "react-router-dom";
+import { useState } from "react";
+import links from "./links";
+
 const Statistics = () => {
+  const { language } = useParams();
+  const [selectedId, setSelected] = useState(links[language].links[0].id);
+
+  const handleLinkSelect = (id) => {
+    setSelected(id);
+  };
+
   return (
     <>
-      <h1>Statistics</h1>
+      <div className="statistics">
+        <div className="container">
+          <h1>{links[language].header}</h1>
+          <div className="links">
+            <ul>
+              {links[language].links.map((link) => {
+                return (
+                  <Link key={link.id} to={link.link}>
+                    <div className="wrapper" key={link.id}>
+                      <li
+                        onClick={() => {
+                          handleLinkSelect(link.id);
+                        }}
+                        className={selectedId === link.id ? "selected" : ""}>
+                        {link.name}
+                      </li>
+                    </div>
+                  </Link>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+        <Outlet />
+      </div>
     </>
   );
 };
