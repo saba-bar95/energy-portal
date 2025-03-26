67cad7903f5855292e89224666e5600cd5e1d520
saba-bar95 <s.barbakadze95@gmail.com>
2025-03-26 10:20:04 +0400

    add small changes in visualization page

--- src/assets/components/Section/Sections/Visualization/links.js
+++ src/assets/components/Section/Sections/Visualization/links.js
@@ -2,22 +2,22 @@ const links = {
   ka: {
     header: "ენერგეტიკული ბალანსის ვიზუალიზაცია",
     links: [
-      { name: "ენერგორესურსების მიხედვით", id: 1, link: "resources" },
-      { name: "ნაკადების მიხედვით", id: 2, link: "streams" },
+      { name: "ენერგორესურსი", id: 1, link: "resources" },
+      { name: "ნაკადი", id: 2, link: "flows" },
       { name: "ენერგეტიკული მაჩვენებლები", id: 3, link: "indicators" },
       { name: "ენერგეტიკული ბალანსის ცხრილები", id: 4, link: "tables" },
-      { name: "დაკადების დიაგრამა", id: 5, link: "diagram" },
+      { name: "ნაკადების დიაგრამა", id: 5, link: "diagram" },
     ],
   },
 
   en: {
     header: "Visualization of energy balance",
     links: [
-      { name: "By energy resources", id: 1, link: "resources" },
-      { name: "By streams", id: 2, link: "streams" },
+      { name: "Energy resources", id: 1, link: "resources" },
+      { name: "By flow", id: 2, link: "flows" },
       { name: "Energy indicators", id: 3, link: "indicators" },
       { name: "Energy balance tables", id: 4, link: "tables" },
-      { name: "Diagram of the deck", id: 5, link: "diagram" },
+      { name: "Energy flow diagram ", id: 5, link: "diagram" },
     ],
   },
 };
--- src/assets/components/Section/Sections/Visualization/visualizationRoutes.jsx
+++ src/assets/components/Section/Sections/Visualization/visualizationRoutes.jsx
@@ -1,15 +1,16 @@
 import { Navigate } from "react-router-dom";
 import Resources from "../Visualization/Pages/Resources/Resources";
-import Streams from "../Visualization/Pages/Streams/Streams";
+import Flows from "./Pages/Flows/Flows";
 import Indicators from "../Visualization/Pages/Indicators/Indicators";
 import Tables from "../Visualization/Pages/Tables/Tables";
 import Diagram from "../Visualization/Pages/Diagram/Diagram";
 import resourcesRoutes from "./Pages/Resources/resourcesRoutes";
+import flowsRoutes from "./Pages/Flows/flowsRoutes";
 
 const consumptionRoutes = [
   { path: "", element: <Navigate to="resources" replace /> }, // Default to heating
   { path: "resources", element: <Resources />, children: resourcesRoutes },
-  { path: "streams", element: <Streams /> },
+  { path: "flows", element: <Flows />, children: flowsRoutes },
   { path: "indicators", element: <Indicators /> },
   { path: "tables", element: <Tables /> },
   { path: "diagram", element: <Diagram /> },
