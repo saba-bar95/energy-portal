4f7e6dcf9c14a591639adec8b1973622806710e3
saba-bar95 <s.barbakadze95@gmail.com>
2025-04-11 11:48:29 +0400

    remove extra files

--- src/assets/components/Section/Sections/Visualization/Pages/Flows/Pages/Export/Export.jsx
+++
@@ -1,8 +0,0 @@
-import "./Export.scss";
-import Chart_1 from "./Chart_1";
-
-const Export = () => {
-  return <Chart_1 />;
-};
-
-export default Export;
--- src/assets/components/Section/Sections/Visualization/Pages/Flows/Pages/Import/Import.jsx
+++
@@ -1,8 +0,0 @@
-import "./Import.scss";
-import Chart_1 from "./Chart_1";
-
-const Import = () => {
-  return <Chart_1 />;
-};
-
-export default Import;
