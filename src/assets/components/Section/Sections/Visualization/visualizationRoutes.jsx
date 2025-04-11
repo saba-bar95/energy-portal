ae690e80ddb9ea1a5dc2c64677f86a126ddbd60a
saba-bar95 <s.barbakadze95@gmail.com>
2025-04-11 11:57:55 +0400

    add table routes

--- src/assets/components/Section/Sections/Visualization/visualizationRoutes.jsx
+++ src/assets/components/Section/Sections/Visualization/visualizationRoutes.jsx
@@ -6,13 +6,14 @@ import Tables from "../Visualization/Pages/Tables/Tables";
 import Diagram from "../Visualization/Pages/Diagram/Diagram";
 import resourcesRoutes from "./Pages/Resources/resourcesRoutes";
 import flowsRoutes from "./Pages/Flows/flowsRoutes";
+import tableRoutes from "./Pages/Tables/tablesRoutes";
 
 const consumptionRoutes = [
-  { path: "", element: <Navigate to="resources" replace /> }, // Default to heating
+  { path: "", element: <Navigate to="resources" /> }, // Default to heating
   { path: "resources", element: <Resources />, children: resourcesRoutes },
   { path: "flows", element: <Flows />, children: flowsRoutes },
   { path: "indicators", element: <Indicators /> },
-  { path: "tables", element: <Tables /> },
+  { path: "tables", element: <Tables />, children: tableRoutes },
   { path: "diagram", element: <Diagram /> },
 ];
 
