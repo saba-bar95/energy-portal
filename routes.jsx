9a1747163932ffa46b1631a378e3378b0a23ea20
saba-bar95 <s.barbakadze95@gmail.com>
2025-04-15 12:56:16 +0400

    add indicator routes

--- routes.jsx
+++ routes.jsx
@@ -4,12 +4,14 @@ import { Navigate } from "react-router-dom";
 import Visualization from "./src/assets/components/Section/Sections/Visualization/Visualization";
 import Statistics from "./src/assets/components/Section/Sections/Statistics/Statistics";
 import Consumption from "./src/assets/components/Section/Sections/Consumption/Consumption";
+import Indicators from "./src/assets/components/Section/Sections/Indicators/Indicators";
 import HomePage from "./src/assets/components/HomePage/HomePage";
 import LanguageCheck from "./LanguageCheck";
 import ErrorPage from "./src/assets/components/ErrorPage/ErrorPage";
 import consumptionRoutes from "./src/assets/components/Section/Sections/Consumption/consumptionRoutes";
 import visualizationRoutes from "./src/assets/components/Section/Sections/Visualization/visualizationRoutes";
 import statisticsRoutes from "./src/assets/components/Section/Sections/Statistics/statisticsRoutes";
+import indicatorsRoutes from "./src/assets/components/Section/Sections/Indicators/indicatorsRoutes";
 
 const routes = [
   { path: "/", element: <Navigate to="/ge" /> }, // Redirect to /ge
@@ -36,6 +38,11 @@ const routes = [
         path: "consumption",
         element: <Consumption />,
         children: consumptionRoutes, // Use the defined consumption routes
+      },
+      {
+        path: "indicators",
+        element: <Indicators />,
+        children: indicatorsRoutes, // Use the defined consumption routes
       },
     ],
   },
