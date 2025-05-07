d7fbb2c111011d8d9e6ef27c69b34f8ebe844470
saba-bar95 <s.barbakadze95@gmail.com>
2025-05-07 17:26:31 +0400

    adjust some responsive styles

--- src/assets/components/HomePage/Slider/Slider.jsx
+++ src/assets/components/HomePage/Slider/Slider.jsx
@@ -40,60 +40,62 @@ const Slider = () => {
 
   return (
     <div className="slider">
-      <div className="top-side">
-        <div className="dots">
-          {slides.map((_, index) => (
-            <div
-              key={index}
-              className={`dot ${currentIndex === index ? "active" : ""}`}
-              onClick={() => setCurrentIndex(index)} // Allow clicking on dots to navigate
-            />
-          ))}
-        </div>
-        <div className="slide">
-          <h2>{slides[currentIndex][`header_${language}`]} - 2023</h2>
-        </div>
-        <div className="navigation">
-          <button
-            onClick={prevSlide}
-            className={currentIndex === 0 ? "disabled" : ""}
-            disabled={currentIndex === 0}>
-            <svg
-              width="18"
-              height="16"
-              viewBox="0 0 18 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M8.2 1L0.999999 8M0.999999 8L8.2 15M0.999999 8L17 8"
-                stroke="#1E1E1E"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+      <div className="slider-container">
+        <div className="top-side">
+          <div className="dots">
+            {slides.map((_, index) => (
+              <div
+                key={index}
+                className={`dot ${currentIndex === index ? "active" : ""}`}
+                onClick={() => setCurrentIndex(index)} // Allow clicking on dots to navigate
               />
-            </svg>
-          </button>
-          <button
-            onClick={nextSlide}
-            className={currentIndex === slides.length - 1 ? "disabled" : ""}
-            disabled={currentIndex === slides.length - 1}>
-            <svg
-              width="18"
-              height="16"
-              viewBox="0 0 18 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M9.8 15L17 8M17 8L9.8 1M17 8L1 8"
-                stroke="#1E1E1E"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
+            ))}
+          </div>
+          <div className="slide">
+            <h2>{slides[currentIndex][`header_${language}`]} - 2023</h2>
+          </div>
+          <div className="navigation">
+            <button
+              onClick={prevSlide}
+              className={currentIndex === 0 ? "disabled" : ""}
+              disabled={currentIndex === 0}>
+              <svg
+                width="18"
+                height="16"
+                viewBox="0 0 18 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M8.2 1L0.999999 8M0.999999 8L8.2 15M0.999999 8L17 8"
+                  stroke="#1E1E1E"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+            <button
+              onClick={nextSlide}
+              className={currentIndex === slides.length - 1 ? "disabled" : ""}
+              disabled={currentIndex === slides.length - 1}>
+              <svg
+                width="18"
+                height="16"
+                viewBox="0 0 18 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M9.8 15L17 8M17 8L9.8 1M17 8L1 8"
+                  stroke="#1E1E1E"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="content-container">
-        {slideContents[currentIndex]} {/* Render the corresponding content */}
+        <div className="content-container">
+          {slideContents[currentIndex]} {/* Render the corresponding content */}
+        </div>
       </div>
     </div>
   );
