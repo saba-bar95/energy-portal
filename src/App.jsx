043c56d2e736d5d1bfd6acaae7ea0fa1f6089ace
saba-bar95 <s.barbakadze95@gmail.com>
2025-11-04 16:56:21 +0400

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./assets/components/Header/Header";
 import Footer from "./assets/components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Outlet />
       </main>
       <Footer />
+      <Analytics />
     </>
   );
 }
